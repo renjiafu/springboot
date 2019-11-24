b51e63c4dfedaa706a3f7148ac2882f74a802fb5
konix <1546500177@qq.com>
2019-11-24 10:12:47 +0800

    注解版本

--- springboot-mybatis/springboot-annotation/src/main/java/com/rjf/MybatisAnnotationApp.java
+++ springboot-mybatis/springboot-annotation/src/main/java/com/rjf/MybatisAnnotationApp.java
@@ -1,12 +1,16 @@
 package com.rjf;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rjf.mapper")
 public class MybatisAnnotationApp {
+
     public static void main(String[] args){
         SpringApplication.run(MybatisAnnotationApp.class,args);
     }
+
 }
