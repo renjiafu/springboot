8d32d57d6c5dc6cacb90f7bf4128e29ca7d6b1a3
konix <1546500177@qq.com>
2019-11-24 08:13:18 +0800

    搭建框架

---
+++ springboot-mybatis/springboot-annotation/src/main/java/com/rjf/MybatisAnnotationApp.java
@@ -0,0 +1,12 @@
+package com.rjf;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisAnnotationApp {
+    public static void main(String[] args){
+        SpringApplication.run(MybatisAnnotationApp.class,args);
+    }
+}
