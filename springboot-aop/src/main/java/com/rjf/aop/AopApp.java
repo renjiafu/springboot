32e7a3dbb36af455771144143ca84c27b5d89160
Rene <1546500177@qq.com>
2020-03-24 21:24:41 +0800

    aop

---
+++ springboot-aop/src/main/java/com/rjf/aop/AopApp.java
@@ -0,0 +1,14 @@
+package com.rjf.aop;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AopApp.class,args);
+    }
+}
---
+++ springboot-aop/src/main/java/com/rjf/aop/aspect/AopControllerAspect.java
@@ -0,0 +1,27 @@
+package com.rjf.aop.aspect;
+
+import com.rjf.aop.web.AopController;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Aspect
+@Service
+public class AopControllerAspect {
+
+    static Logger logger = LoggerFactory.getLogger(AopController.class);
+
+    @Pointcut("execution(* com.rjf.aop.web.AopController.test(..))")
+    public void aopControllerAspect(){
+    }
+
+    @Around("aopControllerAspect()")
+    public void around(){
+        logger.warn("环绕通知 !!!");
+    }
+
+
+}
