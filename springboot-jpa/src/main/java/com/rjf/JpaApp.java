73f3424b2dfbfe8fe9f117707a0f9716bcdca105
Rene <1546500177@qq.com>
2020-06-20 22:58:20 +0800

    初步框架搭建

---
+++ springboot-jpa/src/main/java/com/rjf/JpaApp.java
@@ -0,0 +1,18 @@
+package com.rjf;
+
+/*
+ *
+ *   Rene
+ *   2020/6/20 22:53
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApp.class);
+    }
+}
---
+++ springboot-jpa/src/main/java/com/rjf/model/UserDTO.java
@@ -0,0 +1,11 @@
+package com.rjf.model;
+
+/*
+ *
+ *   Rene
+ *   2020/6/20 22:54
+ */
+
+
+public class UserDTO {
+}
