f2db210b06b15279e58fc3d7d81e57962fa0f696
Rene <1546500177@qq.com>
2020-07-01 07:35:58 +0800

    es

---
+++ springboot-es/src/main/java/com/rjf/com/EsApp.java
@@ -0,0 +1,17 @@
+package com.rjf.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ *
+ *   Rene
+ *   2020/7/1 7:35
+ */
+@SpringBootApplication
+public class EsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApp.class);
+    }
+}
---
+++ springboot-es/src/test/java/com/rjf/EsTest.java
@@ -0,0 +1,15 @@
+package com.rjf;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/*
+ *
+ *   Rene
+ *   2020/7/1 7:31
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsTest {
+}
