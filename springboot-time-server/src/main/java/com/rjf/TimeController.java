c88b1249c927344c241739d2c3267fb0c911ec29
Rene <1546500177@qq.com>
2020-10-17 07:08:51 +0800

    time-server

--- springboot-time-server/src/main/java/com/rjf/TimeController.java
+++ springboot-time-server/src/main/java/com/rjf/TimeController.java
@@ -1,15 +1,19 @@
 package com.rjf;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
 @RestController
 public class TimeController {
 
     @GetMapping("/")
-    public String getTime(){
+    public String getTime(HttpServletRequest req, HttpServletResponse res) {
         return LocalDateTime.now().toString();
     }
 }
